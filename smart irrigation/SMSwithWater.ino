e3f91d9891b4b056f073cfcb9f8d6f28afc7e3d7
Chandrika-Saha <chandrika.cse1.bu@gmail.com>
2020-11-05 19:14:19 +0600

    Update SMSwithWater.ino

--- smart irrigation/SMSwithWater.ino
+++ smart irrigation/SMSwithWater.ino
@@ -1,172 +1,117 @@
-/*********
-  Complete project details at http://randomnerdtutorials.com  
-*********/
-
 #include <SoftwareSerial.h>
-int readv;
-int in = A0;
+int readv;  //var for storing the value read from water indicator
+int in = A0; //input pin for water indicator
+String incoming_char=""; //stor text message
+int led = 13; //led high pin
+int relay = 12;
+int gsm = 9;
+int f=0;
+int ff=0;
+SoftwareSerial SIM900(7, 8);  //configuring serial input
 
-char incoming_char=0;
-int led = 13;
-int flag=0;
-String text = "";
-// Configure software serial port
-SoftwareSerial SIM900(7, 8); 
-
-void setup() {
-  // Arduino communicates with SIM900 GSM shield at a baud rate of 19200
-  // Make sure that corresponds to the baud rate of your module
-  SIM900.begin(19200);
-  // Give time to your GSM shield log on to network
-  delay(20000);   
-    pinMode(in,INPUT);
+void setup() 
+{
+  digitalWrite(gsm, HIGH);
+  delay(1000);
+  digitalWrite(gsm, LOW);
+  delay(5000);
+  
+  SIM900.begin(19200); // sim900 communicates at 19200 speed
+  delay(20000);   // setup time for sim900
+  pinMode(in,INPUT);
   Serial.begin(19200);
-  // Send the SMS
- // sendSMS();
-
   pinMode(led,OUTPUT);
-  // Arduino communicates with SIM900 GSM shield at a baud rate of 19200
-  // Make sure that corresponds to the baud rate of your module
- // SIM900.begin(19200);
-  // For serial monitor
-  //Serial.begin(19200); 
-  // Give time to your GSM shield log on to network
-  //delay(20000);
-  // AT command to set SIM900 to SMS mode
-  SIM900.print("AT+CMGF=1\r"); 
+  digitalWrite(relay,HIGH);
+  pinMode(relay, OUTPUT);
+  SIM900.print("AT+CMGF=1\r");   // AT command to set SIM900 to SMS mode
   delay(100);
-  // Set module to send SMS data to serial out upon receipt 
-  SIM900.print("AT+CNMI=2,2,0,0,0\r");
+  SIM900.print("AT+CNMI=2,2,0,0,0\r"); // Set module to send SMS data to serial out upon receipt
   delay(100);
- 
 }
 
-void loop() { 
-
+void loop() 
+{ 
    readv = analogRead(in);
-   if(readv > 230)
+   if(readv > 230 && f==0) //if water level cross the limit 
    {
-    sendSMSHigh();
-    Serial.println("KODU");
-    delay(20000);
-   }
-
-  if(readv < 80)
-   {
-    sendSMSLow();
-    Serial.println("MODU");
-    delay(20000);
+      f=1;
+      sendSMSHigh(); //motor turn OFF sms sent to mobile
+      Serial.println("OVERFLOW"); 
    }
    
-  Serial.print("Level: ");
-  Serial.println(readv);
-  //delay(1000);
-  if(SIM900.available() >0) {
-    //Get the character from the cellular serial port
-    //incoming_char=SIM900.read(); 
-    incoming_char = SIM900.read();
-    //Print the incoming character to the terminal
-    Serial.print(incoming_char); 
-    text = String(incoming_char+text); 
-  }
+   if(readv < 80 && ff==0) //if the water level lowered to a critical level
+   {
+      ff = 1;
+      sendSMSLow(); //motor turn ON sms sent to mobile
+      Serial.println("UNDERFLOW");
+   }
+   
+   // Serial.print("Level: ");
+   //Serial.println(readv);
 
-  int i;
-  for(i=0;i<text.length();i=i+1)
-  {
-    if(text[i]=='#')
+    
+    if(SIM900.available() > 0)
     {
-      flag = 1;
-    break;
+      incoming_char = SIM900.readString(); //Get the character from the cellular serial port
+      Serial.print(incoming_char); //Print the incoming character to the terminal
     }
-  }
+    
 
-  if(flag ==1)
-  {  
-     digitalWrite(led,HIGH);
-     
-
-     Serial.println(text);
-     Serial.println("Kodu Ajad");
-     flag = 0;
-     text="";
-     SIM900.flush();
-  }
-
-  for(i=0;i<text.length();i=i+1)
-  {
-    if(text[i]=='*')
+    if(incoming_char.indexOf("#ON")>=0)//if the string contains "#ON"
     {
-      flag = 2;
-      break;
+      digitalWrite(relay, LOW);
+      digitalWrite(led,HIGH); //turn on motor
+      Serial.println("MOTOR ON");
+      f = 0; 
+      incoming_char=""; //re-initialize string to store 
+      SIM900.flush(); //clearing sim900's buffer
     }
-  }
 
-  if(flag ==2)
-  {  
-     digitalWrite(led,LOW);
-     Serial.println(text);
-     Serial.println("Modhu Ajad");
-     flag = 0;
-     text="";
-     SIM900.flush();
-  }
+    
+    if(incoming_char.indexOf("*OFF")>=0)//if the string contains "*OFF"
+    {
+      digitalWrite(relay, HIGH);
+      digitalWrite(led,LOW); //turn on motor
+      ff = 0;
+      Serial.println("MOTOR OFF"); 
+      incoming_char=""; //re-initialize string to store 
+      SIM900.flush(); //clearing sim900's buffer
+    }
   
 }
 
-void sendSMSHigh() {
-  // AT command to set SIM900 to SMS mode
-  SIM900.print("AT+CMGF=1\r"); 
+void sendSMSHigh() 
+{
+  SIM900.print("AT+CMGF=1\r"); // AT command to set SIM900 to SMS mode
   delay(100);
-
-  // REPLACE THE X's WITH THE RECIPIENT'S MOBILE NUMBER
-  // USE INTERNATIONAL FORMAT CODE FOR MOBILE NUMBERS
-  SIM900.println("AT + CMGS = \"+8801521229179\""); 
+  SIM900.println("AT + CMGS = \"+8801521229179\""); //SMS sending number
   delay(100);
-  
-  // REPLACE WITH YOUR OWN SMS MESSAGE CONTENT
-  SIM900.println("Water level CRITICAL..!!!"); 
+  SIM900.println("Water level OVERFLOW..!!! turn OFF the motor."); //SMS content
   delay(100);
-
-  // End AT command with a ^Z, ASCII code 26
-  SIM900.println((char)26); 
+  SIM900.println((char)26);  // End AT command with a ^Z, ASCII code 26
   delay(100);
   SIM900.println();
-  // Give module time to send SMS
-  
-
-   
-    SIM900.print("AT+CMGF=1\r"); 
+  SIM900.print("AT+CMGF=1\r"); 
   delay(100);
-  // Set module to send SMS data to serial out upon receipt 
-  SIM900.print("AT+CNMI=2,2,0,0,0\r");
-  delay(5000);
+  SIM900.print("AT+CNMI=2,2,0,0,0\r");   // Set module to send SMS data to serial out upon receipt 
+  delay(100);
+  
 }
 
-void sendSMSLow() {
-  // AT command to set SIM900 to SMS mode
+void sendSMSLow()
+{
   SIM900.print("AT+CMGF=1\r"); 
   delay(100);
-
-  // REPLACE THE X's WITH THE RECIPIENT'S MOBILE NUMBER
-  // USE INTERNATIONAL FORMAT CODE FOR MOBILE NUMBERS
   SIM900.println("AT + CMGS = \"+8801521229179\""); 
   delay(100);
-  
-  // REPLACE WITH YOUR OWN SMS MESSAGE CONTENT
-  SIM900.println("Turn on the Motor NOWWW!!!"); 
+  SIM900.println("Water level UNDERFLOW..!!Turn on the Motor."); 
   delay(100);
-
-  // End AT command with a ^Z, ASCII code 26
   SIM900.println((char)26); 
   delay(100);
-  SIM900.println();
-  // Give module time to send SMS
-  
-
-   
-    SIM900.print("AT+CMGF=1\r"); 
+  SIM900.println();  
+  SIM900.print("AT+CMGF=1\r"); 
   delay(100);
-  // Set module to send SMS data to serial out upon receipt 
   SIM900.print("AT+CNMI=2,2,0,0,0\r");
-  delay(5000);
+  delay(100);
+  
 }
